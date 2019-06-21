ca99bcba1734e43fb4c0227e5b15c41c9d024cc4
amit rai <aforamitrai@gmail.com>
2019-06-21 16:41:18 +0530

    Updates

---
+++ precache-manifest.ab2d09b587db505f471e034f1781ffd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9feb6b7d60eb1426c8910af248a0c078",
+    "url": "/amitrai/index.html"
+  },
+  {
+    "revision": "610adf5bfb6f5a363f81",
+    "url": "/amitrai/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "c44b846c37ea496b6fcd",
+    "url": "/amitrai/static/js/2.34b4fde6.chunk.js"
+  },
+  {
+    "revision": "610adf5bfb6f5a363f81",
+    "url": "/amitrai/static/js/main.99da34b0.chunk.js"
+  },
+  {
+    "revision": "029c2f1d35a0cea76c5b",
+    "url": "/amitrai/static/js/runtime~main.6aa4b398.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/amitrai/static/media/logo.5d5d9eef.svg"
+  }
+]);
